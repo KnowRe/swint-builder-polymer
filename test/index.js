43fab61425c10ad6fa7c12fa6778bd96510f0c9c
Kivol <kivoloid@gmail.com>
2016-11-22 18:09:43 +0900

    Build: Test doesn't print now

--- test/index.js
+++ test/index.js
@@ -5,7 +5,7 @@ var os = require('os'),
 	swintHelper = require('swint-helper'),
 	buildPolymer = require('../lib');
 
-// global.swintVar.printLevel = 5;
+global.swintVar.printLevel = 5;
 
 describe('builder-polymer', function() {
 	it('Error when no callback', function() {
